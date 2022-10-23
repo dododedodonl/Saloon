3b1ff09c3b66eb7501e379312dfc3783a501d04f
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-10-23 22:24:07 +0100

    Updated docs

--- doctum.php
+++ doctum.php
@@ -2,10 +2,19 @@
 
 use Doctum\Doctum;
 use Symfony\Component\Finder\Finder;
+use Doctum\RemoteRepository\GitHubRemoteRepository;
 
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
     ->in('./src');
 
-return new Doctum($iterator);
+return new Doctum($iterator, [
+    'title' => 'Saloon v1 API',
+    'language' => 'en',
+    'build_dir' => __DIR__ . '/build',
+    'cache_dir' => __DIR__ . '/cache',
+    'source_dir' => './',
+    'remote_repository' => new GitHubRemoteRepository('sammyjo20/saloon', __DIR__),
+    'default_opened_level' => 2, // optional, 2 is the default value
+]);
